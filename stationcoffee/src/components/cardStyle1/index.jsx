bf5557e430c4df0c079a0bc9ec0c4746e834475f
babislz <gabriela.laureano2007@gmail.com>
2024-09-17 15:32:02 -0300

    card

--- stationcoffee/src/components/cardStyle1/index.jsx
+++ stationcoffee/src/components/cardStyle1/index.jsx
@@ -1,8 +1,18 @@
-import { Container } from "./styles";
+import { Container, ProdImage } from "./styles";
+import Delete from "../../../public/delete.svg"
+import Cafe from "../../../public/cafe.jpg"
 
 const CardOrderStyle = () => {
     return(
-        <Container>
+        <Container> 
+            <ProdImage src={Cafe} alt="" />
+            <div>
+                <div>
+                    <h1>Café Tradicional</h1>
+                </div>
+                <img src={Delete} alt="" />
+            </div>
+            <h2></h2>
         </Container>
     )
 }
--- stationcoffee/src/components/cardStyle1/styles.jsx
+++ stationcoffee/src/components/cardStyle1/styles.jsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 25vw;
-    height: 180px;
-    background-color: aqua;
+    width: 26vw;
+    height: 22vh;
+    border-radius: 25px;
+    display: flex;
+    padding: 25px;
+    justify-content: space-between;
+    background-color: #6A5445;
+`
+
+export const ProdImage = styled.img`
+    object-fit: cover;
+    width: 600px;
+    border-radius: 20px;
 `
