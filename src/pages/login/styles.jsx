793bd89a7688565395098241f532ce355f9097ae
babislz <gabriela.laureano2007@gmail.com>
2024-10-29 10:15:02 -0300

    figuring out how to use public on vercel

--- src/pages/login/styles.jsx
+++ src/pages/login/styles.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import loginBackground from "../../assets/login-container.svg";
+import loginBackground from "/login-container.svg";
 
 export const Container = styled.div`
     width: 100vw;
