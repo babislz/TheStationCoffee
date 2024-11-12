cda822ca94064225a11444247ddb3522e26dd844
babislz <gabriela.laureano2007@gmail.com>
2024-11-12 10:43:54 -0300

    test

--- src/components/navbar/styles.jsx
+++ src/components/navbar/styles.jsx
@@ -2,18 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    height: 7vh;
+    height: 8vh;
     top: 0;
     background-color: #1E1916;
     justify-content: space-between;
     align-items: center;
     display: flex;
     position: absolute;
-`
-
-export const Profile = styled.img`
-    position: relative;
-    right: 20px;
-    margin: 0;
-    padding: 0;
 `
